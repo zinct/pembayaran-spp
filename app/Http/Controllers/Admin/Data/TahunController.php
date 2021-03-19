994597fb3f18b46b3f06f8348db3a948232479c5
indramahesa <indramahesa128@gmail.com>
2021-03-19 18:53:00 +0700

    menambahkan data tahun ajaran

---
+++ app/Http/Controllers/Admin/Data/TahunController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\admin\data;
+
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+class TahunController extends BaseController
+{
+    public function index()
+    {
+        $data['tahun'] = \App\Tahun::get();
+        return view('admin/data/tahun/index', $data);
+    }
+
+    public function find(\App\Tahun $tahun)
+    {
+        return $tahun;
+    }
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+        ]);
+
+        $model = new \App\Tahun();
+        $model->nama = $request->nama;
+        $model->save();
+        return redirect()->route('admin.data.tahun.index')->with('success', 'Data Berhasil Ditambahkan');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama' => 'required',
+        ]);
+        
+        $model = \App\Tahun::find($id);
+        $model->nama = $request->nama;
+        $model->save();
+        return redirect()->route('admin.data.tahun.index')->with('success', 'Data Berhasil Ditambahkan');
+    }
+    
+    public function destroy($id)
+    {
+        \App\Tahun::destroy($id);
+        return back()->with('success', 'Data Berhasil Dihapus');
+    }
+}
---
+++ app/Tahun.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tahun extends Model
+{
+    protected $table = 'tahun';
+}
